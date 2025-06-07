0a4ec847dd9ad613bfe03907d47d49d442ac7a66
Yuri Lage <slageyuri@gmail.com>
2025-06-07 14:08:22 -0300

    Movement Rook + handling exceptions

--- src/chess/Pieces/Rook.java
+++ src/chess/Pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.Pieces;
 
 import Boardgame.Board;
+import Boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -13,6 +14,48 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0,0);
+
+        //above the piece
+        p.setValues(position.getRow()-1, position.getColumn()); // -1 to go up in the matrix rows
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+            p.setRow(p.getRow()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()]=true; //piece that can be captured
+        }
+
+        //left
+        p.setValues(position.getRow(), position.getColumn()-1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+            p.setColumn(p.getColumn()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()]=true; //piece that can be captured
+        }
+
+        //right
+        p.setValues(position.getRow(), position.getColumn()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+            p.setColumn(p.getColumn()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()]=true; //piece that can be captured
+        }
+
+        //below
+        p.setValues(position.getRow()+1, position.getColumn());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+            p.setRow(p.getRow()+     1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()]=true; //piece that can be captured
+        }
+
         return mat;
     }
 

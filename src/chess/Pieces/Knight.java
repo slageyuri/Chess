9d3c0de466e683c5deac3958755182e842f499c3
Yuri Lage <slageyuri@gmail.com>
2025-06-09 14:46:45 -0300

    King possible moves

--- src/chess/Pieces/Knight.java
+++ src/chess/Pieces/Knight.java
@@ -1,6 +1,7 @@
 package chess.Pieces;
 
 import Boardgame.Board;
+import Boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -13,6 +14,50 @@ public class Knight extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        //above
+        p.setValues(position.getRow()-1, position.getColumn() );
+        if (getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+        }
+        //below
+        p.setValues(position.getRow()+1, position.getColumn() );
+        if (getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+        }
+        //left
+        p.setValues(position.getRow(), position.getColumn()-1 );
+        if (getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+        }
+        //right
+        p.setValues(position.getRow(), position.getColumn()+1 );
+        if (getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+        }
+        //northwest (up-left)
+        p.setValues(position.getRow()-1, position.getColumn()-1 );
+        if (getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+        }
+        //northeast (up-right)
+        p.setValues(position.getRow()-1, position.getColumn()+1 );
+        if (getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+        }
+        //southwest (down-left)
+        p.setValues(position.getRow()+1, position.getColumn()-1 );
+        if (getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+        }
+        //southeast (down-right)
+        p.setValues(position.getRow()+1, position.getColumn()+1 );
+        if (getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]=true;
+        }
+
         return mat;
     }
 
@@ -20,4 +65,11 @@ public class Knight extends ChessPiece {
     public String toString(){
         return "C";
     }
+    private boolean canMove(Position position){
+        Board board;
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p.getColor()!= getColor();
+
+    }
+
 }
